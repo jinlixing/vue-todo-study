67415eacc585c8ab30cc2ca458fcddb7a8080f87
jinlixing <42017552+jinlixing@users.noreply.github.com>
2018-09-18 15:46:33 +0800

    init for study

---
+++ build/webpack.config.practice.js
@@ -0,0 +1,63 @@
+const path = require('path')
+const webpack = require('webpack')
+const webpackMerge = require('webpack-merge')
+const baseConfig = require('./webpack.config.base')
+const HTMLPlugin = require('html-webpack-plugin')
+
+const defaultPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: '"development"'
+    }
+  }),
+  new HTMLPlugin({
+    template: path.join(__dirname, 'template.html')
+  })
+]
+
+let config
+
+const devServer = {
+  port: 8080,
+  host: '0.0.0.0',
+  overlay: {
+    errors: true
+  },
+  hot: true
+}
+
+config = webpackMerge(baseConfig, {
+  entry: path.join(__dirname, '../practice/index.js'),
+  devtool: '#cheap-module-eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.styl/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          'stylus-loader'
+        ]
+      }
+    ]
+  },
+  devServer,
+  resolve: {
+    // import Vue from 'vue'
+    alias: {
+      'vue': path.join(__dirname, '../node_modules/vue/dist/vue.esm.js')
+    }
+  },
+  plugins: defaultPlugins.concat([
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ])
+})
+
+module.exports = config
